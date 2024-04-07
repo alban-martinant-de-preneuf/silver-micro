aa0b1b11aac9640df9326a233817a28a1ec4b665
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2024-04-07 22:58:24 +0200

    setup user authentication

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({message: "Requête reçue !"});
-})
+const userRoutes = require('./routes/user')
+
+mongoose.connect('mongodb://localhost:27017/silver_micro?retryWrites=true&w=majority',
+    {
+        authSource: 'admin',
+        user: "mongadmin",
+        pass: "password123"
+    })
+    .then(() => console.log('Connexion à MongoDB réussie.'))
+    .catch((e) => console.log('Connexion à MongoBD échouée !', e));
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', "default-src 'self';");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
