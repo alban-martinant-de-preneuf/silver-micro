32ed5aa5e87af385e8a448faae35afe68e4a3822
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2024-04-08 11:44:27 +0200

    auth token in a cookie

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-const User  = require('../models/user');
+const User = require('../models/user');
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 
@@ -21,20 +21,31 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id},
+
+                    res.cookie(
+                        'token',
+                        jwt.sign(
+                            { userId: user._id },
                             process.env.SECRET_KEY,
                             { expiresIn: '24h' }
-                        )
+                        ),
+                        {
+                            httpOnly: true,
+                            secure: true
+                        }
+
+                    )
+
+                    res.status(200).json({
+                        userId: user._id,
+                        message: 'Utilisateur connecté'
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
