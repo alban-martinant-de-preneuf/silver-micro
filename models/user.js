d3c0e5ed5ca52f7e8030d68060c72042e21e423d
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2024-04-08 18:07:38 +0200

    add role

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    roles: { type: String, required: true, default: 'user' }
 })
 
 module.exports = mongoose.model('User', userSchema);
