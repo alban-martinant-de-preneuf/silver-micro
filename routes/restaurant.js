88d5de407ad1efcfc889262f741f485e3a9124b8
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2024-04-10 18:45:53 +0200

    adjust middlewares

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -6,9 +6,8 @@ const tableCtrl = require('../controllers/table');
 const auth = require('../middleware/auth');
 const checks = require('../middleware/checks');
 
-router.get('/', auth, restaurantCtrl.getAllRestaurants);
-router.get('/:id', auth, restaurantCtrl.getOneRestaurant);
-
+router.get('/', restaurantCtrl.getAllRestaurants);
+router.get('/:id', restaurantCtrl.getOneRestaurant);
 router.post('/register', auth, restaurantCtrl.register);
 router.post(
     '/:restaurantId/tables',
@@ -16,20 +15,12 @@ router.post(
     checks.isRestaurantOwner,
     tableCtrl.createTable
 );
-router.get('/:restaurantId/tables', auth, tableCtrl.getAllTablesOfRestaurant);
-
-// router.get('/table/:tableId', auth, tableCtrl.getOneTable);
-
+router.get('/:restaurantId/tables', tableCtrl.getAllTablesOfRestaurant);
 router.post(
     '/:restaurantId/availabilities',
     auth,
     checks.isRestaurantOwner,
     restaurantCtrl.createAvailability
 );
-// todo
-// router.put('/:id', auth, restaurantCtrl.updateRestaurant);
-// router.put('/:id/table/:tableId', auth, tableCtrl.updateTable);
-// router.get('/:id/table', auth, restaurantCtrl.getAllTables);
-// router.get('/:id/table/:tableId', auth, restaurantCtrl.getOneTable);
 
 module.exports = router;
--- routes/table.js
+++ routes/table.js
@@ -3,10 +3,7 @@ const router = express.Router();
 
 const tableCtrl = require('../controllers/table');
 
-const auth = require('../middleware/auth');
-
-router.get('/:id', auth, tableCtrl.getOneTable);
-router.get('/:tableId/availabilities', auth, tableCtrl.getAvailabilities);
-// router.post('/:tableId/reservation', auth, bookingCtrl.reserveTable);
+router.get('/:id', tableCtrl.getOneTable);
+router.get('/:tableId/availabilities', tableCtrl.getAvailabilities);
 
 module.exports = router;
